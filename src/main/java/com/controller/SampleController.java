5fc8fcb689465aedebad9a48800c6ea17b78329b
shabhana <shabhanameathajayaprakash9185@gmail.com>
2022-10-05 07:29:52 +0300

    spring boot with docker

---
+++ src/main/java/com/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	@GetMapping(value="/")
+	public String greeting() {
+			return "welcome to spring boot with docker";
+			
+		}
+		@GetMapping(value="say/{name}")
+		public String SayHello(@PathVariable("name") String name) {
+			return "welcome user "+name+ " to spring boot with docker";
+		}
+	}
+
+
